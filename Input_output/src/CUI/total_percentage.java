dba6d2135e54e446607a6e481c44ffe3a6fd47d6
Swopnil38 <swopnilbista2003@gmail.com>
2022-04-19 20:54:40 +0545

    Completed GUI

---
+++ Input_output/src/CUI/total_percentage.java
@@ -0,0 +1,21 @@
+package CUI;
+
+import java.util.Scanner;
+
+public class total_percentage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter MArk of 1st Subject: ");
+        double a = scanner.nextDouble();
+        System.out.print("Enter MArk of 2nd Subject: ");
+        double b = scanner.nextDouble();
+        System.out.print("Enter MArk of 3rd Subject: ");
+        double c = scanner.nextDouble();
+        double total_mark;
+        double percentage;
+        total_mark = a+b+c;
+        System.out.println("Total Marks is :" + total_mark);
+        percentage = total_mark/3;
+        System.out.println("Total Percentage is :" + percentage);
+    }
+}
